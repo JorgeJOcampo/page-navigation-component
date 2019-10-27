5a4ff7c0888f0bd9e976ff0f612fc6e725d44d2f
Shorshi <jorgejocampo@gmail.com>
2019-10-27 09:48:28 -0300

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PageNavigator from './components/PageNavigator';
+
+function App() {
+  return (
+    <div className="App">
+      <body> 
+        <PageNavigator/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
